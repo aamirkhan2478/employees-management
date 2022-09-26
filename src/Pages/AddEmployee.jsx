480840c32d64558335f681037d4f290a8061c65a
Aamir Khan <aamir.khan2478@gmail.com>
2022-09-26 22:57:20 +0500

    show employees and add employees features added

--- src/Pages/AddEmployee.jsx
+++ src/Pages/AddEmployee.jsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import { Button, Container, TextField } from "@mui/material";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { v4 as uuid } from "uuid";
+import { addEmployees } from "../Redux/Employees/employees";
 
 const AddEmployee = () => {
-  return (
-    <div>AddEmployee</div>
-  )
-}
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    contact: "",
+    address: "",
+  });
+  const dispatch = useDispatch();
 
-export default AddEmployee
+  const changeHandler = (e) => {
+    const { name, value } = e.target;
+    setValues((preData) => {
+      return {
+        ...preData,
+        [name]: value,
+      };
+    });
+  };
+
+  const { name, email, contact, address } = values;
+  const clickHandler = () => {
+    if (name === "" || email === "" || contact === "" || address == "") {
+      alert("Please fill all required fields");
+      return;
+    }
+
+    const newData = {
+      id: uuid(),
+      ...values,
+    };
+    dispatch(addEmployees(newData));
+  };
+  return (
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 4,
+        mt: 3,
+      }}
+    >
+      <TextField
+        label="Name"
+        sx={{ width: 600 }}
+        name="name"
+        value={name}
+        onChange={changeHandler}
+      />
+      <TextField
+        label="Email"
+        sx={{ width: 600 }}
+        name="email"
+        value={email}
+        onChange={changeHandler}
+      />
+      <TextField
+        label="Contact"
+        sx={{ width: 600 }}
+        name="contact"
+        value={contact}
+        onChange={changeHandler}
+      />
+      <TextField
+        label="Address"
+        multiline
+        sx={{ width: 600 }}
+        name="address"
+        value={address}
+        onChange={changeHandler}
+      />
+      <Button variant="contained" sx={{ width: 200 }} onClick={clickHandler}>
+        Submit
+      </Button>
+    </Container>
+  );
+};
+
+export default AddEmployee;
